c687fd82d5890b13b543368005f1e2dd12b9a013
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-07-14 12:14:26 +0300

    14/7/2024

--- src/modules/Tasks/tasks.controller.js
+++ src/modules/Tasks/tasks.controller.js
@@ -2,30 +2,27 @@ import { taskModel } from "../../../database/models/tasks.model.js";
 import ApiFeature from "../../utils/apiFeature.js";
 import catchAsync from "../../utils/middleWare/catchAsyncError.js";
 
-
 const createTask = catchAsync(async (req, res, next) => {
-  if(req.body.users){
-
-  if(req.body.users.length > 1){ 
-req.body.isShared = true;
-req.body.taskType = "shared";
-}
+  if (req.body.users) {
+    if (req.body.users.length > 1) {
+      req.body.isShared = true;
+      req.body.taskType = "shared";
+    }
   }
   let newTask = new taskModel(req.body);
   let addedTask = await newTask.save();
 
   res.status(201).json({
     message: " Task has been created successfully!",
-    addedTask
+    addedTask,
   });
 });
-
 
 const getAllTaskByAdmin = catchAsync(async (req, res, next) => {
   let ApiFeat = new ApiFeature(taskModel.find().populate("users"), req.query)
     .pagination()
     .sort()
-    .search()
+    .search();
 
   let results = await ApiFeat.mongooseQuery;
   results = JSON.stringify(results);
@@ -53,20 +50,26 @@ const getAllTaskByAdmin = catchAsync(async (req, res, next) => {
     });
   }
 
-  res.json({ message: "done", page: ApiFeat.page,count: await taskModel.countDocuments(),
-  results });
+  res.json({
+    message: "done",
+    page: ApiFeat.page,
+    count: await taskModel.countDocuments(),
+    results,
+  });
   if (!ApiFeat) {
     return res.status(404).json({
       message: "No Task was found!",
     });
   }
-
 });
 const getAllTaskByUser = catchAsync(async (req, res, next) => {
-  let ApiFeat = new ApiFeature(taskModel.find({ users: req.params.id }).populate("users"), req.query)
+  let ApiFeat = new ApiFeature(
+    taskModel.find({ users: req.params.id }).populate("users"),
+    req.query
+  )
     .pagination()
     .sort()
-    .search()
+    .search();
 
   let results = await ApiFeat.mongooseQuery;
   results = JSON.stringify(results);
@@ -94,20 +97,34 @@ const getAllTaskByUser = catchAsync(async (req, res, next) => {
     });
   }
 
-  res.json({ message: "done", page: ApiFeat.page,count: await taskModel.countDocuments({ users: req.params.id }),
-  results });
+  res.json({
+    message: "done",
+    page: ApiFeat.page,
+    count: await taskModel.countDocuments({ users: req.params.id }),
+    results,
+  });
   if (!ApiFeat) {
     return res.status(404).json({
       message: "No Task was found!",
     });
   }
-
 });
 const getAllTaskByUserShared = catchAsync(async (req, res, next) => {
-  let ApiFeat = new ApiFeature(taskModel.find(  { $and: [{ users: req.params.id }, { taskType: 'shared' },{isShared:true}] }).populate("users"), req.query)
+  let ApiFeat = new ApiFeature(
+    taskModel
+      .find({
+        $and: [
+          { users: req.params.id },
+          { taskType: "shared" },
+          { isShared: true },
+        ],
+      })
+      .populate("users"),
+    req.query
+  )
     .pagination()
     .sort()
-    .search()
+    .search();
 
   let results = await ApiFeat.mongooseQuery;
   results = JSON.stringify(results);
@@ -134,20 +151,40 @@ const getAllTaskByUserShared = catchAsync(async (req, res, next) => {
     });
   }
 
-  res.json({ message: "done", page: ApiFeat.page,count: await taskModel.countDocuments(  { $and: [{ users: req.params.id }, { taskType: 'shared' },{isShared:true}] }),
-  results });
+  res.json({
+    message: "done",
+    page: ApiFeat.page,
+    count: await taskModel.countDocuments({
+      $and: [
+        { users: req.params.id },
+        { taskType: "shared" },
+        { isShared: true },
+      ],
+    }),
+    results,
+  });
   if (!ApiFeat) {
     return res.status(404).json({
       message: "No Task was found!",
     });
   }
-
 });
 const getAllTaskByUserNormal = catchAsync(async (req, res, next) => {
-  let ApiFeat = new ApiFeature(taskModel.find(  { $and: [{ users: req.params.id }, { taskType: 'normal' },{isShared:false}] }).populate("users"), req.query)
+  let ApiFeat = new ApiFeature(
+    taskModel
+      .find({
+        $and: [
+          { users: req.params.id },
+          { taskType: "normal" },
+          { isShared: false },
+        ],
+      })
+      .populate("users"),
+    req.query
+  )
     .pagination()
     .sort()
-    .search()
+    .search();
 
   let results = await ApiFeat.mongooseQuery;
   results = JSON.stringify(results);
@@ -174,14 +211,23 @@ const getAllTaskByUserNormal = catchAsync(async (req, res, next) => {
     });
   }
 
-  res.json({ message: "done", page: ApiFeat.page,count: await taskModel.countDocuments(  { $and: [{ users: req.params.id }, { taskType: 'shared' },{isShared:true}] }),
-  results });
+  res.json({
+    message: "done",
+    page: ApiFeat.page,
+    count: await taskModel.countDocuments({
+      $and: [
+        { users: req.params.id },
+        { taskType: "shared" },
+        { isShared: true },
+      ],
+    }),
+    results,
+  });
   if (!ApiFeat) {
     return res.status(404).json({
       message: "No Task was found!",
     });
   }
-
 });
 
 const getTaskById = catchAsync(async (req, res, next) => {
@@ -198,66 +244,58 @@ const updateTaskPhoto = catchAsync(async (req, res, next) => {
   let { id } = req.params;
   let resources = "";
   let documments = "";
-if (req.body.documments || req.body.resources) {
-  req.body.documments = req.files.documments.map((file) => `${process.env.BASE_URL}tasks/${file.filename}`);
-  req.body.resources = req.files.resources.map((file) => `${process.env.BASE_URL}tasks/${file.filename}`);
+  if (req.body.documments || req.body.resources) {
+    req.body.documments = req.files.documments.map(
+      (file) => `${process.env.BASE_URL}tasks/${file.filename}`
+    );
+    req.body.resources = req.files.resources.map(
+      (file) => `${process.env.BASE_URL}tasks/${file.filename}`
+    );
 
-  if (req.body.documments) {
-    documments = req.body.documments;
-  }
-  if (req.body.resources) {
-    resources = req.body.resources;
-  }
-}
-  let updatedTask = await taskModel.findByIdAndUpdate(
-    id,
-    { $push: { documments: documments, resources: resources, } },
-    { new: true }
-  );
-
-  if (!updatedTask) {
-    return res
-      .status(404)
-      .json({ message: "Couldn't update!  not found!" });
-  }
-
-  res.status(200).json({ message: "Task updated successfully!",updatedTask });
-});
-const updateTask = 
-catchAsync(
-  async (req, res, next) => {
-  let { id } = req.params;
-  if(req.body.users){
-  if(req.body.users.length > 1){ 
-    req.body.isShared = true;
-    req.body.taskType = "shared";
-
+    if (req.body.documments) {
+      documments = req.body.documments;
+    }
+    if (req.body.resources) {
+      resources = req.body.resources;
     }
   }
   let updatedTask = await taskModel.findByIdAndUpdate(
     id,
-    req.body,
+    { $push: { documments: documments, resources: resources } },
     { new: true }
   );
 
   if (!updatedTask) {
-    return res
-      .status(404)
-      .json({ message: "Couldn't update!  not found!" });
+    return res.status(404).json({ message: "Couldn't update!  not found!" });
   }
 
-  res.status(200).json({ message: "Task updated successfully!",updatedTask });
-}
-);
+  res.status(200).json({ message: "Task updated successfully!", updatedTask });
+});
+const updateTask = catchAsync(async (req, res, next) => {
+  let { id } = req.params;
+  if (req.body.users) {
+    if (req.body.users.length > 1) {
+      req.body.isShared = true;
+      req.body.taskType = "shared";
+    }
+  }
+  let updatedTask = await taskModel.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  if (!updatedTask) {
+    return res.status(404).json({ message: "Couldn't update!  not found!" });
+  }
+
+  res.status(200).json({ message: "Task updated successfully!", updatedTask });
+});
 const deleteTask = catchAsync(async (req, res, next) => {
   let { id } = req.params;
 
-  let deletedTask = await taskModel.findByIdAndDelete({_id:id});
+  let deletedTask = await taskModel.findByIdAndDelete({ _id: id });
 
   if (!deletedTask) {
-    return res
-      .status(404)
-      .json({ message: "Couldn't delete!  not found!" });
+    return res.status(404).json({ message: "Couldn't delete!  not found!" });
   }
 
   res.status(200).json({ message: "Task deleted successfully!" });
@@ -266,11 +304,19 @@ const deleteTask = catchAsync(async (req, res, next) => {
 const addPhotos = catchAsync(async (req, res, next) => {
   let resources = "";
   let documments = "";
-  req.body.documments = req.files.documments.map((file) => `${process.env.BASE_URL}tasks/${file.filename}`);
-  req.body.resources = req.files.resources.map((file) => `${process.env.BASE_URL}tasks/${file.filename}`);
+  req.body.documments =
+    req.files.documments &&
+    req.files.documments.map(
+      (file) => `${process.env.BASE_URL}tasks/${file.filename}`
+    );
+  req.body.resources =
+    req.files.resources &&
+    req.files.resources.map(
+      (file) => `${process.env.BASE_URL}tasks/${file.filename}`
+    );
 
-// console.log(req.body.documments);
-// console.log(req.body.resources);
+  // console.log(req.body.documments);
+  // console.log(req.body.resources);
 
   if (req.body.documments) {
     documments = req.body.documments;
@@ -281,11 +327,20 @@ const addPhotos = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     message: "Photo created successfully!",
-     documments,
-     resources,
+    documments,
+    resources,
   });
 });
 
-
-
-export { createTask, getAllTaskByAdmin, getTaskById, updateTask, deleteTask ,getAllTaskByUser,addPhotos,updateTaskPhoto,getAllTaskByUserShared ,getAllTaskByUserNormal};
+export {
+  createTask,
+  getAllTaskByAdmin,
+  getTaskById,
+  updateTask,
+  deleteTask,
+  getAllTaskByUser,
+  addPhotos,
+  updateTaskPhoto,
+  getAllTaskByUserShared,
+  getAllTaskByUserNormal,
+};
