99922da319a9d9bfd5e6e8b74c67b15b380119cc
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-07-16 14:03:37 +0300

    taskid

--- src/modules/Message/message.controller.js
+++ src/modules/Message/message.controller.js
@@ -7,7 +7,7 @@ const createmessage = catchAsync(async (req, res, next) => {
   const newmessage = new messageModel(req.body);
   const savedmessage = await newmessage.save();
 
-  sio.emit(`message_${req.body.sender}, ${ savedmessage.createdAt },${ req.body.content}`);
+  sio.emit(`message_${req.body.sender}_${req.body.taskId}, ${ savedmessage.createdAt },${ req.body.content}`);
 
 
   res.status(201).json({
