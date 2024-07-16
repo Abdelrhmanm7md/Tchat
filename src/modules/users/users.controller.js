0573c5f98259ff7af4d1932a5786143258a1eb21
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-07-16 14:22:50 +0300

    //////

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -5,47 +5,89 @@ import AppError from "../../utils/appError.js";
 import path from 'path';
 import fsExtra from "fs-extra";
 
-const addPhoto = catchAsync(async (req, res, next) => {
-  if (req.file) req.body.profilePic = req.file.originalname;
+// const addPhoto = catchAsync(async (req, res, next) => {
+//   if (req.file) req.body.profilePic = req.file.originalname;
+//   let profilePic = "";
+//   if (req.body.profilePic) {
+//     profilePic = req.body.profilePic;
+//   }
+
+//   // profilePic = profilePic.map(
+//   //   (file) => `http://localhost:8000/tasks/${file.originalname.split(" ").join("")}`
+//   // );
+
+//     const directoryPath = path.join(profilePic, 'uploads/profilePic');
+    
+//     fsExtra.readdir(directoryPath, (err, files) => {
+//         if (err) {
+//             return console.error('Unable to scan directory: ' + err);
+//         }
+    
+//         files.forEach(file => {
+//             const oldPath = path.join(directoryPath, file);
+//             const newPath = path.join(directoryPath, file.replace(/\s+/g, ''));
+    
+//             fsExtra.rename(oldPath, newPath, (err) => {
+//                 if (err) {
+//                     console.error('Error renaming file: ', err);
+//                 } else {
+//                     console.log(`Renamed: ${file} -> ${file.replace(/\s+/g, '')}`);
+//                 }
+//             });
+//         });
+//     });
+  
+//   if (!req.body.profilePic) {
+//     return res.status(404).json({ message: "Couldn't update!  not found!" });
+//   }
+//   res.status(200).json({
+//     message: "Photo updated successfully!",
+//     // profilePic: `http://localhost:8000/profilePic/${profilePic}`,
+//     profilePic
+//   });
+// });
+
+const addPhotos = catchAsync(async (req, res, next) => {
   let profilePic = "";
+  req.body.profilePic =
+    req.files.profilePic &&
+    req.files.profilePic.map(
+      (file) => `http://localhost:8000/tasks/${file.filename.split(" ").join("")}`
+    );
+
+
+
+    const directoryPath = path.join(profilePic, 'uploads/profilePic');
+    
+    fsExtra.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return console.error('Unable to scan directory: ' + err);
+        }
+    
+        files.forEach(file => {
+            const oldPath = path.join(directoryPath, file);
+            const newPath = path.join(directoryPath, file.replace(/\s+/g, ''));
+    
+            fsExtra.rename(oldPath, newPath, (err) => {
+                if (err) {
+                    console.error('Error renaming file: ', err);
+                } else {
+                    console.log(`Renamed: ${file} -> ${file.replace(/\s+/g, '')}`);
+                }
+            });
+        });
+    });
+    
+
   if (req.body.profilePic) {
     profilePic = req.body.profilePic;
   }
-// profilePic = profilePic.split(" ").join("");
-  // profilePic = profilePic.map(
-  //   (file) => `http://localhost:8000/tasks/${file.originalname.split(" ").join("")}`
-  // );
-
-    // const directoryPath = path.join( 'uploads/profilePic',profilePic);
-    
-    // fsExtra.readdir(directoryPath, (err, files) => {
-    //     if (err) {
-    //         return console.error('Unable to scan directory: ' + err);
-    //     }
-    
-    //     files.forEach(file => {
-    //         const oldPath = path.join(directoryPath, file);
-    //         const newPath = path.join(directoryPath, file.replace(/\s+/g, ''));
-    
-    //         fsExtra.rename(oldPath, newPath, (err) => {
-    //             if (err) {
-    //                 console.error('Error renaming file: ', err);
-    //             } else {
-    //                 console.log(`Renamed: ${file} -> ${file.replace(/\s+/g, '')}`);
-    //             }
-    //         });
-    //     });
-    // });
-  
 
 
-  if (!req.body.profilePic) {
-    return res.status(404).json({ message: "Couldn't update!  not found!" });
-  }
+
   res.status(200).json({
-    message: "Photo updated successfully!",
-    profilePic: `https://tchatpro.com/profilePic/${profilePic}`,
-    // profilePic
+    message: "Photo created successfully!",
+    profilePic,
   });
 });
 
@@ -99,4 +141,4 @@ const deleteUser = catchAsync(async (req, res, next) => {
   res.status(200).json({ message: "User deleted successfully!" });
 });
 
-export { getAllUsersByAdmin, getUserById, updateUser, deleteUser, addPhoto };
+export { getAllUsersByAdmin, getUserById, updateUser, deleteUser, addPhotos };
--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -2,16 +2,22 @@ import express from "express";
 const usersRouter = express.Router();
 
 import * as usersController from "./users.controller.js";
-import { uploadSingleFile } from "../../utils/middleWare/fileUploads.js";
+import { uploadMixFile, uploadSingleFile } from "../../utils/middleWare/fileUploads.js";
 
 usersRouter.get("/", usersController.getAllUsersByAdmin);
 usersRouter.get("/:id", usersController.getUserById);
 usersRouter.put("/:id", usersController.updateUser);
 usersRouter.delete("/:id", usersController.deleteUser);
+// usersRouter.post(
+//   "/image",
+//   uploadSingleFile("profilePic", "profilePic"),
+//   usersController.addPhoto
+// );
 usersRouter.post(
   "/image",
-  uploadSingleFile("profilePic", "profilePic"),
-  usersController.addPhoto
+  uploadMixFile("profilePic", [
+    { name: "profilePic", maxCount: 1},
+  ]),
+  usersController.addPhotos
 );
-
 export default usersRouter;
