dae631bb9ddf2f13c2b377bf75c458e9635147ce
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-07-22 16:35:03 +0300

    fileupload

--- src/utils/middleWare/fileUploads.js
+++ src/utils/middleWare/fileUploads.js
@@ -13,8 +13,8 @@ let options = (folderName) => {
       cb(null, `./uploads/${folderName}`);
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-      cb(null, uniqueSuffix + "-" + file.originalname);
+      // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null,file.originalname);
     },
 
   });
