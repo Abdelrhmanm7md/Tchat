ce7c0de88b1f16d898fb6c741bd5a1bf6ea09419
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-07-25 15:34:10 +0300

    ***/

--- database/models/message.model.js
+++ database/models/message.model.js
@@ -21,7 +21,7 @@ const messageSchema = mongoose.Schema(
         default:''
     },
     docs: {
-        type: String,
+        type: [String],
     },
     sender: {
       type: mongoose.Schema.Types.ObjectId,
